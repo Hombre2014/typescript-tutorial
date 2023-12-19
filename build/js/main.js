34742d81e511c60c49ec4052bf4c2b3c5d57682f
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-12-19 17:31:50 +0100

    Initial commit.

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 'Rockstar';
+album = 1984;
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
