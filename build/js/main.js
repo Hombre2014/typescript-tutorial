9d4c303462567bb14cfbaf7f022a83e8049ba63d
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-12-21 14:26:46 +0100

    Assertions

--- build/js/main.js
+++ build/js/main.js
@@ -1,78 +1,23 @@
 "use strict";
-// interface PostID = stringOrNumber; // It doesn't work
-// Literal types
-let myName; // This is a literal type "Jen", not a string!
-// myName = 'John'; // It does not work
-let userName; // Works with literal types
-userName = 'Jen'; // Works
-// userName = 'Rachel'; // Does not work
-// Functions
-const add = (a, b) => {
-    return a + b;
+// convert to more or less specific types
+let a = 'hello';
+let b = a; // less specific
+let c = a; // more specific
+let d = 'world';
+let e = 'world';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
 };
-const logMsg = (message) => {
-    console.log(message);
-};
-logMsg('Hello');
-logMsg(add(2, 3));
-// logMsg(add('a', 3)); // It does not work
-let subtract = function (c, d) {
-    return c - d;
-};
-// interface mathFunction {
-//   (a: number, b: number): number;
-// } // It works as well
-let multiply = function (c, d) {
-    return c * d;
-};
-logMsg(multiply(2, 3));
-// Optional Parameters
-const addAll = (a, b, c) => {
-    if (typeof c !== 'undefined') {
-        return a + b + c;
-    }
-    return a + b;
-};
-// Default Parameters
-const sumAll = (a, b, c = 2) => {
-    return a + b + c;
-};
-logMsg(addAll(2, 3, 2));
-logMsg(addAll(2, 3));
-logMsg(sumAll(2, 3));
-const sumAllDefault = (a = 10, b, c = 2) => {
-    return a + b + c;
-};
-// You have to specify undefined if the parameter has a default value and it is not the last one.
-logMsg(sumAllDefault(undefined, 3));
-// Rest Parameters
-// The rest params should come at the end
-const total = (a, ...nums) => {
-    return a + nums.reduce((prev, current) => prev + current);
-};
-logMsg(total(1, 2, 3, 4));
-// Never type
-const createError = (errMsg) => {
-    throw new Error(errMsg);
-};
-const infiniteLoop = () => {
-    let i = 1;
-    while (true) {
-        i++;
-        if (i > 100) {
-            break;
-        }
-    }
-};
-// Custom type guard
-const isNumber = (value) => {
-    return typeof value === 'number' ? true : false;
-};
-// Use of never type
-const numberOrString = (value) => {
-    if (typeof value === 'string')
-        return 'string';
-    if (isNumber(value))
-        return 'number';
-    return createError('This should never happen');
-};
+let myVal = addOrConcat(2, 2, 'concat');
+// Be careful, because TS sees no problem, but a string is returned!
+let nextVal = addOrConcat(2, 2, 'concat');
+// 10 as string; // Not possible
+10; // Possible
+// The DOM
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
+const nextImg = document.getElementById('#img'); // Does not work in TSX in React
+img.src;
+myImg.src;
--- src/main.ts
+++ src/main.ts
@@ -1,111 +1,36 @@
-// Type Aliases
-type stringOrNumber = string | number;
+type One = string;
+type Two = string | number;
+type Three = 'hello';
 
-type stringOrNumberArray = (string | number)[];
+// convert to more or less specific types
+let a: One = 'hello';
+let b = a as Two; // less specific
+let c = a as Three; // more specific
 
-type Guitarist = {
-  name?: string;
-  active: boolean;
-  albums: stringOrNumberArray;
+let d = <One>'world';
+let e = <string | number>'world';
+
+const addOrConcat = (
+  a: number,
+  b: number,
+  c: 'add' | 'concat'
+): number | string => {
+  if (c === 'add') return a + b;
+  return '' + a + b;
 };
 
-type UserId = stringOrNumber;
+let myVal: string = addOrConcat(2, 2, 'concat') as string;
 
-// interface PostID = stringOrNumber; // It doesn't work
+// Be careful, because TS sees no problem, but a string is returned!
+let nextVal: number = addOrConcat(2, 2, 'concat') as number;
 
-// Literal types
-let myName: 'Jen'; // This is a literal type "Jen", not a string!
-// myName = 'John'; // It does not work
+// 10 as string; // Not possible
+10 as unknown as string; // Possible
 
-let userName: 'Jen' | 'Cory' | 'Blue'; // Works with literal types
-userName = 'Jen'; // Works
-// userName = 'Rachel'; // Does not work
+// The DOM
+const img = document.querySelector('img')!;
+const myImg = document.getElementById('#img') as HTMLImageElement;
+const nextImg = <HTMLImageElement>document.getElementById('#img'); // Does not work in TSX in React
 
-// Functions
-
-const add = (a: number, b: number): number => {
-  return a + b;
-};
-
-const logMsg = (message: any): void => {
-  console.log(message);
-};
-
-logMsg('Hello');
-logMsg(add(2, 3));
-// logMsg(add('a', 3)); // It does not work
-
-let subtract = function (c: number, d: number): number {
-  return c - d;
-};
-
-type mathFunction = (a: number, b: number) => number;
-// interface mathFunction {
-//   (a: number, b: number): number;
-// } // It works as well
-
-let multiply: mathFunction = function (c, d) {
-  return c * d;
-};
-
-logMsg(multiply(2, 3));
-
-// Optional Parameters
-const addAll = (a: number, b: number, c?: number): number => {
-  if (typeof c !== 'undefined') {
-    return a + b + c;
-  }
-  return a + b;
-};
-
-// Default Parameters
-const sumAll = (a: number, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-
-logMsg(addAll(2, 3, 2));
-logMsg(addAll(2, 3));
-logMsg(sumAll(2, 3));
-
-const sumAllDefault = (a: number = 10, b: number, c: number = 2): number => {
-  return a + b + c;
-};
-
-// You have to specify undefined if the parameter has a default value and it is not the last one.
-logMsg(sumAllDefault(undefined, 3));
-
-// Rest Parameters
-// The rest params should come at the end
-const total = (a: number, ...nums: number[]): number => {
-  return a + nums.reduce((prev, current) => prev + current);
-};
-
-logMsg(total(1, 2, 3, 4));
-
-// Never type
-const createError = (errMsg: string): never => {
-  throw new Error(errMsg);
-};
-
-const infiniteLoop = () => {
-  let i: number = 1;
-  while (true) {
-    i++;
-    if (i > 100) {
-      break;
-    }
-  }
-};
-
-// Custom type guard
-const isNumber = (value: any): boolean => {
-  return typeof value === 'number' ? true : false;
-};
-
-// Use of never type
-const numberOrString = (value: number | string): string => {
-  if (typeof value === 'string') return 'string';
-  if (isNumber(value)) return 'number';
-
-  return createError('This should never happen');
-};
+img.src;
+myImg.src;
